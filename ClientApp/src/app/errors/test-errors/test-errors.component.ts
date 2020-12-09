52a9a87ac79d58426069dec9f64be684c35e78d4
Seb Quelcutti <squelcutti@gmail.com>
2020-12-09 11:20:27 +0000

    Section 7 Item 79 Validation errors

--- ClientApp/src/app/errors/test-errors/test-errors.component.ts
+++ ClientApp/src/app/errors/test-errors/test-errors.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TestErrorsComponent implements OnInit {
   baseUrl = 'https://localhost:5001/';
+  validationErrors: string[] = [];
 
   constructor(private http: HttpClient) { }
 
@@ -48,7 +49,8 @@ export class TestErrorsComponent implements OnInit {
     this.http.post(this.baseUrl + 'account/register', {}).subscribe(response => {
       console.log(response);
     }, error => {
-      console.log(error);
+        console.log(error);
+        this.validationErrors = error;
     })
   }
 }
