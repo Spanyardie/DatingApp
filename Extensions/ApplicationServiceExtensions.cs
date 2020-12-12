338fd0c4b42dfabfab3748dcdea01cbee3a5a142
Seb Quelcutti <squelcutti@gmail.com>
2020-12-12 09:59:01 +0000

    Section 11 Item 125 Configuring Cloudinary in the API

--- Extensions/ApplicationServiceExtensions.cs
+++ Extensions/ApplicationServiceExtensions.cs
@@ -15,6 +15,8 @@ namespace DatingApp.Extensions
         {
             services.AddScoped<ITokenService, TokenService>();
             services.AddScoped<IUserRepository, UserRepository>();
+            services.Configure<CloudinarySettings>(configuration.GetSection("CloudinarySettings"));
+
             services.AddAutoMapper(typeof(AutoMapperProfiles).Assembly);
 
             services.AddDbContext<DataContext>(options =>
