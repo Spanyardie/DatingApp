c364248719997dfb5f22aff4089ed10dc636c905
Seb Quelcutti <squelcutti@gmail.com>
2020-12-12 11:08:27 +0000

    Section 11 Item 129 Using the CreatedAtRoute method

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -33,7 +33,7 @@ namespace DatingApp.Controllers
         }
 
         [Authorize]
-        [HttpGet("{username}")]
+        [HttpGet("{username}", Name = "GetUser")]
         public async Task<ActionResult<MemberDto>> GetUser(string username)
         {
             return await _userRepository.GetMemberAsync(username);
@@ -79,7 +79,9 @@ namespace DatingApp.Controllers
             user.Photos.Add(photo);
 
             if (await _userRepository.SaveAllAsync())
-                return _mapper.Map<PhotoDto>(photo);
+            {
+                return CreatedAtRoute("GetUser", new { username = user.UserName }, _mapper.Map<PhotoDto>(photo));
+            }
 
             return BadRequest("Problem adding photo");
         }
