c0208069b6fca176a9b788dbd1560a548690a712
Seb Quelcutti <squelcutti@gmail.com>
2020-12-17 17:18:49 +0000

    Section 15 Item 184 Adding a Messages controller

---
+++ Controllers/MessagesController.cs
@@ -0,0 +1,55 @@
+﻿using AutoMapper;
+using DatingApp.DTOs;
+using DatingApp.Entities;
+using DatingApp.Extensions;
+using DatingApp.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace DatingApp.Controllers
+{
+    [Authorize]
+    public class MessagesController : BaseApiController
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IMessageRepository _messageRepository;
+        private readonly IMapper _mapper;
+
+        public MessagesController(IUserRepository userRepository, IMessageRepository messageRepository, IMapper mapper)
+        {
+            _userRepository = userRepository;
+            _messageRepository = messageRepository;
+            _mapper = mapper;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<MessageDto>> CreateMessage(CreateMessageDto createMessageDto)
+        {
+            var userName = User.GetUsername();
+
+            if (userName == createMessageDto.RecipientUsername.ToLower())
+                return BadRequest("You cannot send messages to yourself!");
+
+            var sender = await _userRepository.GetUserByUsernameAsync(userName);
+            var recipient = await _userRepository.GetUserByUsernameAsync(createMessageDto.RecipientUsername);
+
+            if (recipient == null) return NotFound();
+
+            var message = new Message
+            {
+                Sender = sender,
+                Recipient = recipient,
+                SenderUsername = sender.UserName,
+                RecipientUsername = recipient.UserName,
+                Content = createMessageDto.Content
+            };
+
+            _messageRepository.AddMessage(message);
+
+            if (await _messageRepository.SaveAllAsync()) return Ok(_mapper.Map<MessageDto>(message));
+
+            return BadRequest("Unable to create Message");
+        }
+    }
+}
