8b531fcea83ce310d45d3c464b8693e2d647f152
Seb Quelcutti <squelcutti@gmail.com>
2020-12-18 16:35:20 +0000

    Section 15 Item 192 Using query params

--- ClientApp/src/app/members/member-detail/member-detail.component.ts
+++ ClientApp/src/app/members/member-detail/member-detail.component.ts
@@ -14,7 +14,7 @@ import { MessageService } from '../../_services/message.service';
   styleUrls: ['./member-detail.component.css']
 })
 export class MemberDetailComponent implements OnInit {
-  @ViewChild('memberTabs') memberTabs: TabsetComponent;
+  @ViewChild('memberTabs', {static: true}) memberTabs: TabsetComponent;
   member: Member;
   galleryOptions: NgxGalleryOptions[];
   galleryImages: NgxGalleryImage[];
@@ -25,6 +25,11 @@ export class MemberDetailComponent implements OnInit {
 
   ngOnInit(): void {
     this.loadMember();
+
+    this.route.queryParams.subscribe(params => {
+      params.tab ? this.selectTab(params.tab) : this.selectTab(0);
+    })
+
     this.galleryOptions = [
       {
         width: '500px',
@@ -67,5 +72,9 @@ export class MemberDetailComponent implements OnInit {
     if (this.activeTab.heading == 'Messages' && this.messages.length === 0) {
       this.loadMessages();
     }
+  }
+
+  selectTab(tabId: number) {
+    this.memberTabs.tabs[tabId].active = true;
   }
 }
