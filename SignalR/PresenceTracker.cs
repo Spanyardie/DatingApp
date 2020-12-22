4c48665f097c5095700ad3e708d7ef98d1fdaf4d
Seb Quelcutti <squelcutti@gmail.com>
2020-12-22 18:03:02 +0000

    Section 17 Item 223 Adding a Presence tracker

---
+++ SignalR/PresenceTracker.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.SignalR
+{
+    public class PresenceTracker
+    {
+        private static readonly Dictionary<string, List<string>> OnlineUsers = new Dictionary<string, List<string>>();
+
+        public Task UserConnected(string username, string connectionID)
+        {
+            lock(OnlineUsers)
+            {
+                if(OnlineUsers.ContainsKey(username))
+                {
+                    OnlineUsers[username].Add(connectionID);
+                }
+                else
+                {
+                    OnlineUsers.Add(username, new List<string> { connectionID });
+                }
+            }
+
+            return Task.CompletedTask;
+        }
+
+        public Task UserDisconnected(string username, string connectionID)
+        {
+            lock(OnlineUsers)
+            {
+                if (!OnlineUsers.ContainsKey(username)) return Task.CompletedTask;
+
+                OnlineUsers[username].Remove(connectionID);
+                if(OnlineUsers[username].Count == 0)
+                {
+                    OnlineUsers.Remove(username);
+                }
+            }
+
+            return Task.CompletedTask;
+        }
+
+        public Task<string[]> GetOnlineUsers()
+        {
+            string[] onlineUsers;
+            lock(OnlineUsers)
+            {
+                onlineUsers = OnlineUsers.OrderBy(k => k.Key).Select(k => k.Key).ToArray();
+            }
+
+            return Task.FromResult(onlineUsers);
+        }
+    }
+}
